e54e6ea6b16c5f54e2aa65731a47be0effa8a776
SpiritCroc <dev@spiritcroc.de>
2019-05-09 15:53:44 +0200

    roomservice: allow adding remove-project
    Change-Id: I3c2c34618c6cdf5b60a3dede99710c147c251c8e

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -206,13 +206,16 @@ def add_to_manifest(repos, fallback_branch=None):
 _fetch_dep_cache = []
 
 
-def fetch_dependencies(repo_path, fallback_branch=None):
+def fetch_dependencies(repo_path, fallback_branch=None, first_dependency=False):
     global _fetch_dep_cache
     if repo_path in _fetch_dep_cache:
         return
     _fetch_dep_cache.append(repo_path)
 
     print('Looking for dependencies')
+
+    if first_dependency:
+        os.system('vendor/nusantara/build/tools/roomcleaner.py %s' % repo_path)
 
     dep_p = '/'.join((repo_path, custom_dependencies))
     if os.path.exists(dep_p):
@@ -287,7 +290,7 @@ def main():
     if depsonly:
         repo_path = get_from_manifest(device)
         if repo_path:
-            fetch_dependencies(repo_path)
+            fetch_dependencies(repo_path, None, True)
         else:
             print("Trying dependencies-only mode on a "
                   "non-existing device tree?")
@@ -333,7 +336,7 @@ def main():
         os.system('repo sync --force-sync --no-tags --current-branch --no-clone-bundle %s' % repo_path)
         print("Repository synced!")
 
-        fetch_dependencies(repo_path, fallback_branch)
+        fetch_dependencies(repo_path, fallback_branch, True)
         print("Done")
         sys.exit()
 
