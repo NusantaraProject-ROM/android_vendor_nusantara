ee425bb3ef5f6d60880a6da4647674389d62d302
Genkzsz11 <Genkzsz11@gmail.com>
2019-10-15 23:16:36 +0700

    support: convert to android.bp https://github.com/BlissRoms/platform_vendor_support/commit/fcfdd30607359d18104070361cdc53cd13355857

--- support/common.mk
+++ support/common.mk
@@ -13,44 +13,15 @@
 #   include vendor/support/common.mk
 #
 
-ifeq ($(LOCAL_USE_AAPT2),true)
-LOCAL_STATIC_ANDROID_LIBRARIES += \
-    android-support-annotations \
-    androidx.preference_preference \
-    androidx.recyclerview_recyclerview \
-    androidx.cardview_cardview \
-    androidx.appcompat_appcompat \
-    VendorSupportLib
-else
-LOCAL_RESOURCE_DIR += $(call my-dir)/res
-
-
-## Include transitive dependencies below
-
-# Include support-v7-appcompat, if not already included
-ifeq (,$(findstring androidx.appcompat_appcompat,$(LOCAL_STATIC_JAVA_LIBRARIES)))
-LOCAL_RESOURCE_DIR += frameworks/support/appcompat/res
-LOCAL_AAPT_FLAGS += --extra-packages androidx.appcompat_appcompat
-LOCAL_STATIC_JAVA_LIBRARIES += androidx.appcompat_appcompat
-endif
-
-# Include support-v7-recyclerview, if not already included
-ifeq (,$(findstring androidx.recyclerview_recyclerview,$(LOCAL_STATIC_JAVA_LIBRARIES)))
-LOCAL_RESOURCE_DIR += frameworks/support/recyclerview/res
-LOCAL_AAPT_FLAGS += --extra-packages androidx.recyclerview_recyclerview
-LOCAL_STATIC_JAVA_LIBRARIES += androidx.recyclerview_recyclerview
-endif
-
-# Include android-support-v14-preference, if not already included
-ifeq (,$(findstring androidx.preference_preference,$(LOCAL_STATIC_JAVA_LIBRARIES)))
-LOCAL_RESOURCE_DIR += frameworks/support/preference/res
-LOCAL_AAPT_FLAGS += --extra-packages androidx.preference_preference
-LOCAL_STATIC_JAVA_LIBRARIES += androidx.preference_preference
-endif
-
-LOCAL_AAPT_FLAGS += --auto-add-overlay --extra-packages com.dirtyunicorns.support
-
 LOCAL_STATIC_JAVA_LIBRARIES += \
-    android-support-annotations \
+    androidx.annotation_annotation \
+    androidx.lifecycle_lifecycle-common
+
+LOCAL_STATIC_ANDROID_LIBRARIES += \
+    androidx.legacy_legacy-support-v4 \
+    androidx.lifecycle_lifecycle-runtime \
+    androidx.recyclerview_recyclerview \
+    androidx.preference_preference \
+    androidx.appcompat_appcompat \
+    androidx.legacy_legacy-preference-v14 \
     VendorSupportLib
-endif
