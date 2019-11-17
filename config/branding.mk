488a63b5c970d5eb30d4243721f1a95961ed6be5
ezio84 <brabus84@gmail.com>
2019-11-17 23:03:51 +0100

    Use a custom build date-time prop to refresh packages cache  [2/2]
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -5,12 +5,14 @@ DU_BUILD_TYPE := Durex
 # Set all versions
 BUILD_DATE := $(shell date -u +%Y%m%d)
 BUILD_TIME := $(shell date -u +%H%M)
+BUILD_DATE_TIME := $(BUILD_TIME)$(BUILD_DATE)
 DU_VERSION := $(DU_BUILD_TYPE)-$(DU_BASE_VERSION)-$(DU_BUILD)-$(BUILD_DATE)-$(BUILD_TIME)
 TARGET_BACON_NAME := $(DU_VERSION)
 ROM_FINGERPRINT := Durex/$(PLATFORM_VERSION)/$(DU_BASE_VERSION)/$(BUILD_DATE)$(BUILD_TIME)
 
 DU_PROPERTIES := \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
+    ro.build.datetime=$(BUILD_DATE_TIME) \
     ro.du.build.date=$(BUILD_DATE) \
     ro.du.version=$(DU_VERSION) \
     ro.du.fingerprint=$(ROM_FINGERPRINT)
