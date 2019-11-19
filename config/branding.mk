c9d976db8efc02df939c1d61e28400035b49fce3
Genkzsz11 <Genkzsz11@gmail.com>
2019-11-20 02:32:01 +0700

    Branding DUREX

--- config/branding.mk
+++ config/branding.mk
@@ -1,30 +1,16 @@
 # Versioning System
-DU_BASE_VERSION = v14.0
-
-ifndef DU_BUILD_TYPE
-    DU_BUILD_TYPE := UNOFFICIAL
-endif
-
-# Only include DU-Updater for official, weeklies, and rc builds
-ifeq ($(filter-out OFFICIAL WEEKLIES RC,$(DU_BUILD_TYPE)),)
-    PRODUCT_PACKAGES += \
-        DU-Updater
-endif
-
-# Sign builds if building an official or weekly build
-ifeq ($(filter-out OFFICIAL WEEKLIES,$(DU_BUILD_TYPE)),)
-    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(KEYS_LOCATION)
-endif
+DU_BASE_VERSION = Beta
+DU_BUILD_TYPE := Durex
 
 # Set all versions
 BUILD_DATE := $(shell date -u +%Y%m%d)
 BUILD_TIME := $(shell date -u +%H%M)
-DU_VERSION := $(TARGET_PRODUCT)-$(DU_BASE_VERSION)-$(BUILD_DATE)-$(BUILD_TIME)-$(DU_BUILD_TYPE)
+DU_VERSION := $(DU_BUILD_TYPE)-$(DU_BASE_VERSION)-$(DU_BUILD)-$(BUILD_DATE)-$(BUILD_TIME)
 TARGET_BACON_NAME := $(DU_VERSION)
-ROM_FINGERPRINT := DirtyUnicorns/$(PLATFORM_VERSION)/$(DU_BUILD_TYPE)/$(BUILD_DATE)$(BUILD_TIME)
+ROM_FINGERPRINT := Durex/$(PLATFORM_VERSION)/$(DU_BASE_VERSION)/$(BUILD_DATE)$(BUILD_TIME)
 
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+DU_PROPERTIES := \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
+    ro.du.build.date=$(BUILD_DATE) \
     ro.du.version=$(DU_VERSION) \
-    ro.mod.version=$(DU_BUILD_TYPE)-$(DU_BASE_VERSION)-$(BUILD_DATE) \
     ro.du.fingerprint=$(ROM_FINGERPRINT)
