312ad2ccc82e5f55a7b49c09648e6db501d5c4a1
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-18 18:36:05 +0000

    vendor: Allow providing camera server and service
    Change-Id: I6d56930f4673cf7bdf99fccb87c15f0c0fc9a1d7

--- config/BoardConfigDU.mk
+++ config/BoardConfigDU.mk
@@ -1,6 +1,12 @@
 include vendor/du/config/BoardConfigKernel.mk
+include vendor/du/config/BoardConfigSoong.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/du/config/BoardConfigQcom.mk
 endif
-include vendor/du/config/BoardConfigSoong.mk
+
+ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/av/camera/cameraserver \
+    frameworks/av/services/camera/libcameraservice
+endif
