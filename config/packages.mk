dec6c88bf4661b6a5ec2c95cb71cf66d32b27d34
James Taylor <jmz.taylor16@gmail.com>
2020-01-08 11:41:53 -0500

    Build sqlite3
    Change-Id: I9ca846d7f47d321a063ed003ae1634bd87bdb4ff
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -8,3 +8,7 @@ PRODUCT_PACKAGES += \
     messaging \
     WallpaperPicker2
 
+# Allows registering device to Google easier for gapps
+# Integrates package for easier Google Pay fixing
+PRODUCT_PACKAGES += \
+    sqlite3
