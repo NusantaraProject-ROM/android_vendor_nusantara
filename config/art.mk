3d6dff3040c4a9f276854183bf43ca7dd680a271
mydongistiny <jaysonedson@gmail.com>
2020-02-22 20:06:33 -0800

    art: Set a few more default properties fot ART
    Change-Id: Ibecad231e5d78585f7a289f54d06271b28d719ff
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config/art.mk
+++ config/art.mk
@@ -17,6 +17,13 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 PRODUCT_DEXPREOPT_QUICKEN_APPS += \
     Dialer
 
+# Boot image profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
+# System server compiler
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Dexopt boot types
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     pm.dexopt.first-boot=extract \
@@ -29,3 +36,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     pm.dexopt.ab-ota=speed-profile \
     pm.dexopt.inactive=verify \
     pm.dexopt.shared=speed
+
+# dex2oat threads (default)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    dalvik.vm.dex2oat-threads=8 \
+    dalvik.vm.boot-dex2oat-threads=8
