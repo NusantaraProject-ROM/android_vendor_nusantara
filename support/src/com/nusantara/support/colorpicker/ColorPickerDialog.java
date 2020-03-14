d0222ebf42096225c26545b35a0edbae962235ff
00day0 <therandomuser11@gmail.com>
2020-03-14 13:28:46 +0100

    ColorPicker: make it more user friendly
    Show RGB value instead of ARGB because most average users dont even know about alpha in hex color values
    Make edittext limited to 7 chars and one line
    Remove enter imagebutton, it confused me at first time and confirm entered hex with enter key instead
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- support/src/com/nusantara/support/colorpicker/ColorPickerDialog.java
+++ support/src/com/nusantara/support/colorpicker/ColorPickerDialog.java
@@ -27,12 +27,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
+import android.view.KeyEvent;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.nusantara.support.R;
 
-public class ColorPickerDialog extends AlertDialog implements ColorPickerView.OnColorChangedListener, View.OnClickListener {
+public class ColorPickerDialog extends AlertDialog implements ColorPickerView.OnColorChangedListener, View.OnClickListener, View.OnKeyListener {
 
     private ColorPickerView mColorPicker;
     private ColorPickerPanelView mOldColor;
@@ -65,6 +66,15 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
         }
     }
 
+    private void setColorFromHex() {
+        String text = mHex.getText().toString();
+        try {
+            int newColor = ColorPickerPreference.convertToColorInt(text);
+            mColorPicker.setColor(newColor, true);
+        } catch (Exception ignored) {
+        }
+    }
+
     private void setUp(int color) {
 
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(
@@ -85,7 +95,6 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
         mYellow = layout.findViewById(R.id.yellow_panel);
 
         mHex = layout.findViewById(R.id.hex);
-        ImageButton mSetButton = layout.findViewById(R.id.enter);
 
         ((LinearLayout) mOldColor.getParent()).setPadding(Math.round(mColorPicker.getDrawingOffset()),
                 0, Math.round(mColorPicker.getDrawingOffset()), 0);
@@ -104,17 +113,8 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
         setColorAndClickAction(mYellow, 0xffffc90f);
 
         if (mHex != null) {
-            mHex.setText(ColorPickerPreference.convertToARGB(color));
-        }
-        if (mSetButton != null) {
-            mSetButton.setOnClickListener(v -> {
-                String text = mHex.getText().toString();
-                try {
-                    int newColor = ColorPickerPreference.convertToColorInt(text);
-                    mColorPicker.setColor(newColor, true);
-                } catch (Exception e) {
-                }
-            });
+            mHex.setText(ColorPickerPreference.convertToRGB(color));
+            mHex.setOnKeyListener(this);
         }
 
         setView(layout);
@@ -126,7 +126,7 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
         mNewColor.setColor(color);
         try {
             if (mHex != null) {
-                mHex.setText(ColorPickerPreference.convertToARGB(color));
+                mHex.setText(ColorPickerPreference.convertToRGB(color));
             }
         } catch (Exception e) {
         }
@@ -159,6 +159,16 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
 
     public int getColor() {
         return mColorPicker.getColor();
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+            setColorFromHex();
+            return true;
+        }
+        return false;
     }
 
     @Override
