37d428ff2701bf8cc36bfb3501a85830e3a38443
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-02 20:39:48 -0300

    vendor: Build StitchImage
    Change-Id: I2a27c4a2aa5857b4dad3885b08995b5647c4b237

--- config/packages.mk
+++ config/packages.mk
@@ -8,7 +8,8 @@ PRODUCT_PACKAGES += \
     messaging \
     WallpaperPicker2 \
     ThemePicker \
-    TouchGestures
+    TouchGestures \
+    StitchImage
 
 # Allows registering device to Google easier for gapps
 # Integrates package for easier Google Pay fixing
