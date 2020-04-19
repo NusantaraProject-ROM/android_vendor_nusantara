748054689cecf7da6541c8c08a1f15dc762cbabe
Genkzsz11 <Genkzsz11@gmail.com>
2020-04-19 11:25:06 +0700

    Revert "[2/4] Port face unlock feature"
    This reverts commit 17c8c509a9489e781a0d921386b5158e9abe7063.

--- config/common.mk
+++ config/common.mk
@@ -66,15 +66,6 @@ PRODUCT_PACKAGES += \
     mkfs.ntfs \
     mount.ntfs
 
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := false
-ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
-PRODUCT_PACKAGES += \
-    FaceUnlockService
-TARGET_FACE_UNLOCK_SUPPORTED := true
-endif
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
 
 # Art
 include vendor/du/config/art.mk
