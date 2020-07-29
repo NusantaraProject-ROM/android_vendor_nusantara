46a864b7b0ae271e78cd146cb0ba20ed60c062d5
Genkzsz11 <Genkzsz11@gmail.com>
2020-07-29 17:05:46 +0700

    Add official support
    Change-Id: Ifcbc6de7d8e76bf9119ab6c69fee63527be5437e

--- config/branding.mk
+++ config/branding.mk
@@ -1,15 +1,16 @@
 NAD_VERSION_BASE := 1.0
+NAD_BUILD_TYPE ?= UNOFFICIAL
 
 # Set all versions
 BUILD_DATE := $(shell date -u +%d%m%Y)
 BUILD_TIME := $(shell date -u +%H%M)
 BUILD_DATE_TIME := $(BUILD_TIME)$(BUILD_DATE)
-ROM_FINGERPRINT := Nusantara/$(NAD_VERSION_BASE)/$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_DATE)/$(BUILD_TIME)
+ROM_FINGERPRINT := Nusantara/$(NAD_VERSION_BASE)/$(PLATFORM_VERSION)/$(NAD_BUILD_TYPE)/$(BUILD_ID)/$(BUILD_DATE)/$(BUILD_TIME)
 
 ifeq ($(USE_GAPPS), true)
-    NAD_VERSION := Nusantara_v$(NAD_VERSION_BASE)-$(NAD_BUILD)-Gapps-$(BUILD_DATE)-$(BUILD_TIME)
+    NAD_VERSION := Nusantara_v$(NAD_VERSION_BASE)-$(NAD_BUILD)-Gapps-$(BUILD_DATE)-$(NAD_BUILD_TYPE)-$(BUILD_TIME)
 else
-    NAD_VERSION := Nusantara_v$(NAD_VERSION_BASE)-$(NAD_BUILD)-$(BUILD_DATE)-$(BUILD_TIME)
+    NAD_VERSION := Nusantara_v$(NAD_VERSION_BASE)-$(NAD_BUILD)-$(BUILD_DATE)-$(NAD_BUILD_TYPE)-$(BUILD_TIME)
 endif
 
 NAD_PROPERTIES := \
@@ -17,5 +18,6 @@ NAD_PROPERTIES := \
     ro.build.datetime=$(BUILD_DATE_TIME) \
     ro.nad.build.date=$(BUILD_DATE) \
     ro.nad.version=$(NAD_VERSION) \
+    ro.nad.build.type=$(NAD_BUILD_TYPE) \
     ro.nad.build.version=$(NAD_VERSION_BASE) \
     ro.nad.fingerprint=$(ROM_FINGERPRINT)
