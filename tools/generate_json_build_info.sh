d15a106a4c52ff233246a59e4fd86269059a12e1
Genkzsz11 <Genkzsz11@gmail.com>
2020-08-09 23:47:30 +0700

    tools: Use ro.build.datetime
    Change-Id: If180505692af542a9fae714d8357b5aa680cb013

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -6,7 +6,7 @@ then
   if [ -f $file_path ]; then
     file_size=$(stat -c%s $file_path)
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
-    datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
+    datetime=$(grep ro\.build\.datetime $OUT/system/build.prop | cut -d= -f2);
     version=$(grep ro\.nad\.build\.version $OUT/system/build.prop | cut -d= -f2);
     device=$(grep ro\.nad\.device $OUT/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
