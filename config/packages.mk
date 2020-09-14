602b73b379e612389cf42ee66bdba4ad066da5ff
Genkzsz11 <Genkzsz11@gmail.com>
2020-09-14 10:43:26 +0700

    lavender: Disable sqlite3 for now

--- config/packages.mk
+++ config/packages.mk
@@ -22,5 +22,5 @@ PRODUCT_PACKAGES += \
 
 # Allows registering device to Google easier for gapps
 # Integrates package for easier Google Pay fixing
-PRODUCT_PACKAGES += \
-    sqlite3
+#PRODUCT_PACKAGES += \
+#    sqlite3
