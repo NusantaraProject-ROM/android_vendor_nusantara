a89cdae5180ebc958f5c86aece59ee63b89d1f6c
Genkzsz11 <Genkzsz11@gmail.com>
2020-09-15 16:57:13 +0700

    support: disable hostname preference support for now

--- support/src/com/nusantara/support/preferences/HostnamePreference.java
+++ support/src/com/nusantara/support/preferences/HostnamePreference.java
@@ -13,7 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
 
 package com.nusantara.support.preferences;
 
@@ -68,4 +67,4 @@ public class HostnamePreference extends EditTextPreference {
         Settings.Secure.putString(getContext().getContentResolver(),
                 Settings.Secure.DEVICE_HOSTNAME, hostname);
     }
-}
+}*/
