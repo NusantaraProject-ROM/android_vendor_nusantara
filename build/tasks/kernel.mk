rio-31/vendor_komodo

vendor: kernel: Use binutils from kernel toolchain with clang
Can be disabled in BoardConfig with TARGET_KERNEL_NO_LLVM_BINUTILS

Change-Id: I8f017f215690ad1a33ee3d9e69c6343fb7e8abab
Signed-off-by: mydongistiny <jaysonedson@gmail.com>
Signed-off-by: DennySPB <dennyspb@gmail.com>
Signed-off-by: Ahmad Thoriq Najahi <najahi@chips-projects.xyz>

build/tasks/kernel.mk
@@ -184,6 +184,14 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
     ifeq ($(KERNEL_CC),)
         KERNEL_CC := CC="$(CCACHE_BIN) clang"
     endif
+    ifneq ($(TARGET_KERNEL_NO_LLVM_BINUTILS),true)
+        KERNEL_CC += AS=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-as
+        KERNEL_CC += AR=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-ar
+        KERNEL_CC += NM=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-nm
+        KERNEL_CC += OBJCOPY=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-objcopy
+        KERNEL_CC += OBJDUMP=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-objdump
+        KERNEL_CC += STRIP=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-strip
+    endif
 endif
 
 ifneq ($(TARGET_KERNEL_MODULES),)

NusantaraProject-ROM/android_vendor_nusantara

Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

build/tasks/kernel.mk
@@ -208,6 +208,14 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
     ifeq ($(KERNEL_LD),)
         KERNEL_LD :=
     endif
+    ifeq ($(TARGET_KERNEL_NO_LLVM_BINUTILS),true)
+        KERNEL_CC += AS=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-as
+        KERNEL_CC += AR=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-ar
+        KERNEL_CC += NM=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-nm
+        KERNEL_CC += OBJCOPY=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-objcopy
+        KERNEL_CC += OBJDUMP=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-objdump
+        KERNEL_CC += STRIP=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-strip
+    endif
 endif
 
 ifneq ($(TARGET_KERNEL_MODULES),)