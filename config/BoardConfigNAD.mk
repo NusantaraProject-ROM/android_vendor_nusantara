8dbb3f5de614d5a4c348977a05aefa8c2e88e01a
goodmeow <harunbam3@gmail.com>
2021-02-28 15:07:58 +0700

    BoardCondfig: add Namespace for fwk-detect

--- config/BoardConfigNAD.mk
+++ config/BoardConfigNAD.mk
@@ -1,6 +1,15 @@
+# Kernel
 include vendor/nusantara/config/BoardConfigKernel.mk
+
+# Soong
 include vendor/nusantara/config/BoardConfigSoong.mk
 
+# QCOM Specific
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/nusantara/config/BoardConfigQcom.mk
 endif
+
+# Namespace for fwk-detect
+TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
+PRODUCT_SOONG_NAMESPACES += \
+    $(TARGET_FWK_DETECT_PATH)/fwk-detect
