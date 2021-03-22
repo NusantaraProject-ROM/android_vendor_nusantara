ab48c2c22de57563ed70e9e4cf67ccafcf5391dc
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2021-03-23 02:51:07 +0700

    vendor: fix path on json OTA generator script

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -3,12 +3,13 @@ if [ "$1" ]
 then
   file_path=$1
   file_name=$(basename "$file_path")
+  out=$(dirname "$file_path")
   if [ -f $file_path ]; then
     file_size=$(stat -c%s $file_path)
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
-    datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
-    version=$(grep ro\.nad\.build\.version $OUT/system/build.prop | cut -d= -f2);
-    device=$(grep ro\.nad\.device $OUT/system/build.prop | cut -d= -f2);
+    datetime=$(grep ro\.build\.date\.utc $out/system/build.prop | cut -d= -f2);
+    version=$(grep ro\.nad\.build\.version $out/system/build.prop | cut -d= -f2);
+    device=$(grep ro\.nad\.device $out/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
     echo "{" > $file_path.json
     echo "  \"error\"          : false," >> $file_path.json
