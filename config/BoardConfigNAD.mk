074d7b36b59e0a9e139b6fee9b341486cce8454a
Genkzsz11 <genkzsz11@nusantaradevs.org>
2021-04-11 16:04:12 +0700

    Revert "vendor: Allow providing camera server and service"
    This reverts commit 312ad2ccc82e5f55a7b49c09648e6db501d5c4a1.
    
    Signed-off-by: Genkzsz11 <genkzsz11@nusantaradevs.org>
    Change-Id: Ib084d9a27a8f8f2be3fe8fd72274db6b966c9044

--- config/BoardConfigNAD.mk
+++ config/BoardConfigNAD.mk
@@ -4,9 +4,3 @@ include vendor/nusantara/config/BoardConfigSoong.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/nusantara/config/BoardConfigQcom.mk
 endif
-
-ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
-PRODUCT_SOONG_NAMESPACES += \
-    frameworks/av/camera/cameraserver \
-    frameworks/av/services/camera/libcameraservice
-endif
