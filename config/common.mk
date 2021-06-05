089510ab08b4e24b8c663435a49ba22923047505
Sam Mortimer <sam@mortimer.me.uk>
2021-06-04 23:25:41 -0700

    Disable extra StrictMode features on all non-engineering builds --also add adb authentication
    Change-Id: Ib052fba60e12497f79913c8030f59af40b6bc32f

--- config/common.mk
+++ config/common.mk
@@ -8,6 +8,17 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+# Disable ADB authentication
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
+
+# Disable extra StrictMode features on all non-engineering builds
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
+else
+# Enable ADB authentication
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+endif
+
 # Additional props
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.debug.alloc=0 \
