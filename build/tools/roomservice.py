Genos2000/vendor_aicp

roomservice: allow adding remove-project
Change-Id: I3c2c34618c6cdf5b60a3dede99710c147c251c8e

build/tools/roomservice.py
@@ -209,13 +209,16 @@ def add_to_manifest(repos, fallback_branch=None):
 _fetch_dep_cache = []
 
 
-def fetch_dependencies(repo_path, fallback_branch=None):
+def fetch_dependencies(repo_path, fallback_branch=None, first_dependency=False):
     global _fetch_dep_cache
     if repo_path in _fetch_dep_cache:
         return
     _fetch_dep_cache.append(repo_path)
 
     print('Looking for dependencies')
+
+    if first_dependency:
+        os.system('vendor/aicp/build/tools/roomcleaner.py %s' % repo_path)
 
     dep_p = '/'.join((repo_path, custom_dependencies))
     if os.path.exists(dep_p):
@@ -324,7 +327,7 @@ def main():
     if depsonly:
         repo_path = get_from_manifest(device)
         if repo_path:
-            fetch_dependencies(repo_path)
+            fetch_dependencies(repo_path, None, True)
         else:
             # Try again without removing underscores
             device = product
@@ -378,7 +381,7 @@ def main():
             os.system('repo sync --force-sync %s' % repo_path)
             print("Repository synced!")
 
-        fetch_dependencies(repo_path, fallback_branch)
+        fetch_dependencies(repo_path, fallback_branch, True)
         print("Done")
         sys.exit()
 

NusantaraProject-ROM/android_vendor_nusantara

build/tools/roomservice.py
@@ -197,13 +197,16 @@ def add_to_manifest(repos, fallback_branch=None):
 
 _fetch_dep_cache = []
 
-def fetch_dependencies(repo_path, fallback_branch=None):
+def fetch_dependencies(repo_path, fallback_branch=None, first_dependency=False):
     global _fetch_dep_cache
     if repo_path in _fetch_dep_cache:
         return
     _fetch_dep_cache.append(repo_path)
 
     print('Looking for dependencies')
+
+    if first_dependency:
+        os.system('vendor/nusantara/build/tools/roomcleaner.py %s' % repo_path)
 
     dep_p = '/'.join((repo_path, custom_dependencies))
     if os.path.exists(dep_p):
@@ -275,7 +278,7 @@ def main():
     if depsonly:
         repo_path = get_from_manifest(device)
         if repo_path:
-            fetch_dependencies(repo_path)
+            fetch_dependencies(repo_path, None, True)
         else:
             print("Trying dependencies-only mode on a "
                   "non-existing device tree?")
@@ -321,7 +324,7 @@ def main():
         os.system('repo sync --force-sync --no-tags --current-branch --no-clone-bundle %s' % repo_path)
         print("Repository synced!")
 
-        fetch_dependencies(repo_path, fallback_branch)
+        fetch_dependencies(repo_path, fallback_branch, True)
         print("Done")
         sys.exit()
 