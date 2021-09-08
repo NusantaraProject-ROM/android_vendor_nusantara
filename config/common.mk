3d8f7c285ff58edd722ba3aa77dcec1575cd7bec
Genkzsz11 <Genkzsz11@gmail.com>
2021-09-08 11:29:39 +0700

    common: Redfin sept
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -101,7 +101,7 @@ PRODUCT_PACKAGES += \
     mount.ntfs
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
 
 # GMS
 ifeq ($(USE_GAPPS), true)
