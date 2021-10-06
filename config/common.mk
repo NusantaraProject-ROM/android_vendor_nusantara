bdcc817544d4454c541a55d8edf4bb20c286de76
Rafiester <rafi.haqqi@gmail.com>
2021-10-06 22:30:11 +0700

    common: Update redfin fingerprint to October 2021
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -101,7 +101,7 @@ PRODUCT_PACKAGES += \
     mount.ntfs
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
 
 # GMS
 ifeq ($(USE_GAPPS), true)
