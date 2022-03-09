a1d6ed2a1402656b9865d9612f5361fa98f293da
goodmeow <harunbam3@gmail.com>
2022-03-10 05:05:02 +0530

    roomservice: change default branch to 12.1
    Change-Id: Ia284d43a3565ca365156433179dc39513c99ac0f

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -43,7 +43,7 @@ except ImportError:
 DEBUG = False
 
 custom_local_manifest = ".repo/local_manifests/local_nad.xml"
-custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', '12')
+custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', '12.1')
 custom_dependencies = "nad.dependencies"
 org_manifest = "device"  # leave empty if org is provided in manifest
 org_display = "NusantaraROM-devices"  # needed for displaying
