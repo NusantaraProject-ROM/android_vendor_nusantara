c09a92ea89fec926a122e206abb4410986915507
genkzsz11 <genkzsz11@gmail.com>
2022-04-26 21:04:11 +0700

    packages: Build Launcher

--- config/packages.mk
+++ config/packages.mk
@@ -15,6 +15,14 @@ PRODUCT_PACKAGES += \
     SimpleDeviceConfig \
     TouchGestures
 
+ifeq ($(PRODUCT_TYPE), go)
+PRODUCT_PACKAGES += \
+    NusantaraLauncherQuickStepGo
+else
+PRODUCT_PACKAGES += \
+    NusantaraLauncherQuickStep
+endif
+
 # Allows registering device to Google easier for gapps
 # Integrates package for easier Google Pay fixing
 PRODUCT_PACKAGES += \
